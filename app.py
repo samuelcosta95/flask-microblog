394dcb82c15ae47b448ebb4cf02b883c78f84b01
Samuel Costa <samuelcostalf95@gmail.com>
2025-01-02 11:44:19 -0300

    corrigindo erros

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
- 
+
 def create_app():
     app = Flask(__name__)
-    client = MongoClient(os.getenv("MONGO_URI"))
-    app.db = client.cluster0
+    client = MongoClient(os.getenv("MONGODB_URI"))
+    app.db = client.microblog
 
     @app.route('/', methods=['GET', 'POST'])
     def home():
